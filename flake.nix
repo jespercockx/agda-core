eae40776493b52d3edf3be40b66e1070a95edf07
Bohdan <b.liesnikov+git@gmail.com>
2024-01-12 12:08:50 +0100

    move agda derivation to a separate file
    fix mkAgdaDerivation
    
    prettify mkAgdaDerivation

--- flake.nix
+++ flake.nix
@@ -19,39 +19,10 @@
 
   outputs = {self, nixpkgs, flake-utils, agda2hs-src, scope-src}:
     let
-      getAttrOrDefault = atr: def: set:
-        if builtins.hasAttr atr set then builtins.getAttr atr set else def;
     in (flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        agdaDerivation = args:
-          pkgs.agdaPackages.mkDerivation
-            (args // (if # don't override if Everything is present
-                         args ? everythingFile ||
-                         # don't override if there's an explicit buildPhase
-                         (getAttrOrDefault "buildPhase" null args != null) ||
-                         # do override if either tcFiles or tcDir is present
-                         ! (args ? tcFiles || args ? tcDir)
-                      then builtins.trace "not overriding" {}
-                      else
-                        {buildPhase =
-                           let ipaths = getAttrOrDefault "includePaths" [] args;
-                               concatMapStrings = pkgs.lib.strings.concatMapStrings;
-                               iarg = concatMapStrings (path: "-i" + path + " ") ipaths;
-                           in if args ? tcFiles
-                              then
-                                ''
-                                runHook preBuild
-                                ${concatMapStrings (f: "agda " + iarg + f + ";") args.tcFiles}
-                                runHook postBuild
-                                ''
-                              else
-                                ''
-                                runHook preBuild
-                                find "${args.tcDir}" -type f -name "*.agda" -print0 | xargs -0 -n1 ${"agda" + iarg}
-                                runHook postBuild
-                                ''
-                         ;}));
+        agdaDerivation = pkgs.callPackage ./nix/mkAgdaDerivation.nix {};
         agda2hslib = agdaDerivation
           { pname = "agda2hs";
             meta = {};
